7502d8bcfc75affe1b3e28c518019acd85724854
jaroslaw gryko <jaroslaw.gryko@gmail.com>
2017-02-14 23:27:13 +0100

    Add actions for adding/removind hobbies and movies

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,8 +8,43 @@ var stateDefault = {
     todos: []
 };
 var reducer = (state = stateDefault, action) => {
-    return state;
+    switch (action.type) {
+        case 'CHANGE_SEARCH_TEXT':
+            return{
+                ...state,
+                searchText: action.searchText
+            }
+        default:
+            return state;
+    }
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// subscribe to changes
+store.subscribe(()=>{
+   var state = store.getState();
+
+    document.getElementById('app').innerHTML = state.searchText;
+});
 
 console.log('currentState', store.getState());
+
+
+store.dispatch({
+   type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'work'
+});
+
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'dog'
+});
+
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'Something else'
+});
+
+console.log('searchText should be "worek"',store.getState());
